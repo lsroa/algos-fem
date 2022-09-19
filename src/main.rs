14abf260c3f511edec0226d67ecd9d6c3ae10365
Luis Roa Castro <luis.roa@mac.local>
2022-09-19 12:12:27 +0200

    feat: binary search

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,16 @@ use std::fmt::Display;
 
 fn main() {
     let result = search(&[1, 2, 3, 4], 8);
+    let result_2 = binary_search(&[1, 2, 3, 4, 7, 10, 14, 15], 14);
 
     match result {
         Ok(value) => println!("{}", value),
-        Err(error) => println!("{} in:", error),
+        Err(error) => println!("{}", error),
+    }
+
+    match result_2 {
+        Ok(value) => println!("{}", value),
+        Err(error) => println!("value {} not found", error),
     }
 }
 
@@ -17,4 +23,28 @@ fn search<T: PartialEq + Display>(arr: &[T], value: T) -> Result<usize, String> 
     }
 
     return Err(format!("the value {} not found ", value));
+}
+
+fn binary_search(arr: &[usize], value: usize) -> Result<usize, String> {
+    let mut lo = 0;
+    let mut hi = arr.len();
+
+    loop {
+        let m = lo + (hi - lo) / 2;
+        let v = arr[m];
+
+        if v == value {
+            return Ok(m);
+        } else if value < v {
+            hi = m;
+        } else {
+            lo = m + 1;
+        }
+
+        if hi < lo {
+            break;
+        };
+    }
+
+    return Err(format!("{}", value));
 }
